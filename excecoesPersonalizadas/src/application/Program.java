17811b45e3b563e2c3f0c89b92ab688446538b4b
Leonardo Huck <leonardomonteirohuck@gmail.com>
2025-07-15 19:16:35 -0300

    Solução ruim

--- excecoesPersonalizadas/src/application/Program.java
+++ excecoesPersonalizadas/src/application/Program.java
@@ -33,15 +33,11 @@ public class Program {
             System.out.print("Check-out date (DD/MM/YYYY): ");
             checkOut = LocalDate.parse(scanner.next(), formatter);
 
-            LocalDate now = LocalDate.now();
-            if (checkIn.isBefore(now) || checkOut.isBefore(now)){
-                System.out.println("Error in reservation: Reservation dates for update must be future dates");
-            }
-            else  if (!checkOut.isAfter(checkIn)){
-                System.out.println("Error in reservation: Check-out must be after check-in date");
-            }
-            else {
-                reservation.updateDates(checkIn, checkOut);
+            String error = reservation.updateDates(checkIn, checkOut);
+
+            if (error != null){
+                System.out.println("Error in reservation: " + error);
+            }else {
                 System.out.println("Reservation: " + reservation);
             }
         }
