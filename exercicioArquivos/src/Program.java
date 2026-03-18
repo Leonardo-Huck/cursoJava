7c6518ddeeb9df2bb061c4a67fa9e32e555dabea
Leonardo Huck <leonardomonteirohuck@gmail.com>
2026-03-17 21:49:23 -0300

    Minha solução para o exercício

--- exercicioArquivos/src/Program.java
+++ exercicioArquivos/src/Program.java
@@ -1,8 +1,5 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
+import java.util.*;
 
 
 public class Program {
@@ -12,10 +9,11 @@ public class Program {
         System.out.println("Informe o caminho do arquivo: ");
         String strPath = scanner.nextLine();
 
-        ///home/leonardo/Java/cursoJava/exercicioArquivos/venda.csv
+        ///home/leonardo/Java/cursoJava/exercicioArquivos/src/venda.csv
 
         File path = new File(strPath);
-        boolean success = new File(strPath + "/out").mkdir();
+
+        boolean success = new File(path.getParent() + "/out").mkdir();
         System.out.println("Directory created successfully: " + success);
 
         List<String> listaDeProdutos = new ArrayList<>();
@@ -23,6 +21,16 @@ public class Program {
         try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             String line = br.readLine();
             while (line != null) {
+                String[] temporario;
+                temporario = line.split(",");
+
+                String produto = temporario[0];
+                double valor = Double.parseDouble(temporario[1]);
+                int quantidade = Integer.parseInt(temporario[2]);
+
+                Double total = valor * quantidade;
+
+                listaDeProdutos.add(produto + "," + String.format(Locale.US,"%.2f", total));
 
                 line = br.readLine();
             }
@@ -30,10 +38,10 @@ public class Program {
             System.out.println("Error: " + e.getMessage());
         }
 
-        String summary = path.getPath() + "/out/summary.csv";
+        String summary = path.getParent() + "/out/summary.csv";
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(summary))) {
-            for (String line : lines) {
-                bw.write(line);
+            for (String produto : listaDeProdutos) {
+                bw.write(produto);
                 bw.newLine();
             }
         } catch (IOException e) {
